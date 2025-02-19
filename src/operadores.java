d1c259bdc3e527ece72fca75e61df1c8acfe2e53
Fabiola-FOFS <auproportio@gmail.com>
2025-02-19 16:43:42 -0300

    add operadores java

---
+++ src/operadores.java
@@ -0,0 +1,52 @@
+/*public class Operadores1 {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        int a = 10;
+        int b = 20;
+
+        // Operador de adição
+        int soma = a + b;
+        System.out.println("A soma de " + a + " e " + b + " é: " + soma);
+
+        // Operador de subtração
+        int subtracao = a - b;
+        System.out.println("A subtração de " + a + " e " + b + " é: " + subtracao);
+
+        // Operador de multiplicação
+        int multiplicacao = a * b;
+        System.out.println("A multiplicação de " + a + " e " + b + " é: " + multiplicacao);
+
+        // Operador de divisão
+        int divisao = b / a;
+        System.out.println("A divisão de " + b + " por " + a + " é: " + divisao);
+
+        // Operador de módulo
+        int modulo = b % a;
+        System.out.println("O resto da divisão de " + b + " por " + a + " é: " + modulo);
+
+        // Concatenando strings
+        String nome = "João";
+        String sobrenome = "Silva";
+        String nomeCompleto = nome + " " + sobrenome;
+        System.out.println("Nome completo: " + nomeCompleto)
+
+    }
+}*/
+
+ //concatenação estring com numeros
+ public class Operadores {
+    public static void main(String[] args) {
+        String concatenacao ="?";
+
+        concatenacao = "1" +1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+    }
+ }
+ 
